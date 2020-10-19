7fa22c92f10f6111b5739ca6654bc1242a303b73
dimpho0101 <dimpho@LAPTOP-TD93OO7J.localdomain>
2020-10-19 21:03:39 +0200

    heruko deployment

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.post('/webhook', (req, res) => {  
  
     let body = req.body;
+
+    if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+        return res.sendStatus(204);
+      }
+    
+      return next();
   
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
